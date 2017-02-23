5ec1e955223ac6bd3cf3719444ec202c53f77c8a
Mario Beltrán Alarcón <belco90@gmail.com>
2017-02-23 21:46:25 +0100

    Str method for output file

--- hashcode_videos/videos.py
+++ hashcode_videos/videos.py
@@ -13,6 +13,8 @@ class Problem:
 
 
 class Solution(Individual):
+    END_OF_LINE = "\n"
+
     def __init__(self, problem, cache):
         self.problem = problem
         self.cache = cache
@@ -56,10 +58,57 @@ class Solution(Individual):
 
     def __str__(self):
         # Output file
-        pass
+        output_str = ""
+        cache_used_len = 0
+
+        # Each of the subsequent N lines should describe the videos cached in a single cache server
+        for row_id in range(len(self.cache)):
+            row = self.cache[row_id]
+
+            # ID of the cache server being described, the IDs of the videos stored in this cache server
+            line_data = self._get_cache_row_data(row)
+
+            if len(line_data) > 0:
+                # Sum 1 to num of caches used and prepend cache id
+                cache_used_len += 1
+                line_data.insert(0, str(row_id))
+                output_str += self._new_str_line(line_data)
+
+        # Line containing a single number N - the number of cache server descriptions to follow
+        output_str = self._new_str_line([str(cache_used_len), ]) + output_str
+
+        return output_str
+
+    @staticmethod
+    def _get_cache_row_data(cache_row):
+        line_data = []
+        for col_id in range(len(cache_row)):
+            col_val = cache_row[col_id]
+
+            # Add video id
+            if col_val == 1:
+                line_data.append(str(col_id))
+
+        return line_data
+
+    def _new_str_line(self, line):
+        # Build new line for str with corresponding end of line symbol
+        return ' '.join(line) + self.END_OF_LINE
 
 
 class SolutionSet(Population):
     def __init__(self, problem, size, *args, **kwargs):
         self.problem = problem
         self.individuals = [Solution.random(problem) for _ in range(size)]
+
+
+if __name__ == '__main__':
+    import numpy
+    zer = numpy.zeros((3, 5))
+    zer[0][2] = 1
+    zer[1][3] = 1
+    zer[1][1] = 1
+    zer[2][0] = 1
+    zer[2][1] = 1
+    oc = Solution(None, zer)
+    print(str(oc))
